bd61c7f044f9fcc2a0320147d7d98a51ca823951
JafniDev <faruqjafni@gmail.com>
2025-07-24 19:14:43 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -7,58 +7,193 @@ const page3btn=document.querySelector("#page3btn");
 const hamBtn = document.querySelector("#hamIcon");
 const navigation = document.querySelector("nav");
 var allpage =document.querySelectorAll(".page");
-var backiconBtns =document.querySelectorAll(".backIcon");
+const clickSound = new Audio('audio/Click.wav');
+const posterSound = new Audio('audio/posterClick.wav');
+const pageSound = new Audio('audio/pageClick.wav');
 
-//Var for Minigame
-const minigameWindow=document.querySelector("#Minigame");
-const resultscreen=document.querySelector("#resultScreen");
-const character =document.querySelector("#character");
-const startMinigameButton=document.querySelector("#PlayMinigame");
-const AnswersBtn = document.querySelectorAll(".AnswerBtn");
-const imgAnswers = document.querySelectorAll(".AnswerBtn img");
-const questions = document.querySelector("#questions");
-const scoretext = document.querySelector("#score");
-const correctPrompt = document.querySelector("#correct");
-const wrongPrompt = document.querySelector("#wrong");
-const ranktext = document.querySelector("#displayRank");
-const totalscoretext = document.querySelector("#displayScore");
-var scorevalue;
-var turns;
-var correctAnswerId; //use this to set the correct answer
-var randomNum; //randomizeQuestion
-var buttonIsSet = false;
-let idleInterval; // to store the interval ID
+//var for page 2
+const pizzaNametext=document.querySelector("#pizzanameInfo");
+const crustInfotext=document.querySelector("#crustInfo");
+const toppingInfotext=document.querySelector("#toppingInfo");
 
+const backBtn=document.querySelector("#backBtn");
+const nextBtn=document.querySelector("#nextBtn");
 
-//Var for Shows about Pizza
+var onePizzabtn=document.querySelectorAll(".pizzaBtn");
+var onePizzaImg=document.querySelector("#pizzaImg");
+var index;
+var pizzaIndex = 1;
+
+//var for page 3
 var poster =document.querySelectorAll(".poster");
 const showTitle =document.querySelector("#ShowTitle");
 const showDesc =document.querySelector("#ShowDesc");
-const showImage = document.querySelector('#CurrentShow img');
-var minigameHasStarted = false;
+const showImage = document.querySelector('#CurrentShow');
+
+
 
 //Init
-minigameWindow.style.display = "none";
-resultscreen.style.display = "none";
-correctPrompt.style.display = "none";
-wrongPrompt.style.display = "none";
 
-PlayImageFade();
-startMinigameButton.addEventListener("click", startMinigame);
-
-//set all BackToHomeButtons and PosterButtons
-for (let onebackbtn of backiconBtns)
-{
-onebackbtn.addEventListener("click",hideallandShowMinigameButton);
+for (let i = 0; i < onePizzabtn.length; i++) {
+  onePizzabtn[i].addEventListener("click", function () {
+    DisplayThisPizzaInfo(i + 1);
+    
+  });
 }
 
-for (let i = 0; i <= 5; i++) {
+ nextBtn.addEventListener("click", function () {
+    NextPizza();
+  });
+
+  backBtn.addEventListener("click", function () {
+    BackPizza();
+  });
+
+  for (let i = 0; i <= 5; i++) {
   poster[i].addEventListener("click", function(){DisplayThisPoster(i)});
 }
 
 
+
+
+
+
+
+
 //This function is to change the Images, Title, desc in the Shows aboout Pizza Page
+
+
+function hideall(){ //function to hide all pages
+    for (let onepage of allpage){
+      onepage.style.display="none";
+    }
+}
+
+
+
+
+function showPage(pagenum){
+    hideall();
+    let onepage = document.querySelector("#page" + pagenum);
+    onepage.style.display="flex";
+   pageSound.play();
+    
+    
+}
+
+
+
+
+/*Listen for clicks on the buttons, assign anonymous
+eventhandler functions to call show function*/
+page1btn.addEventListener("click", function () {
+showPage(1);
+});
+page2btn.addEventListener("click", function () {
+showPage(2);
+
+});
+page3btn.addEventListener("click", function () {
+showPage(3);
+
+});
+hideall(); //call hideall function to hide all pages
+
+
+
+const menuItemsList=document.querySelector("nav ul");
+hamBtn.addEventListener("click",toggleMenus);
+
+function toggleMenus(){ 
+
+menuItemsList.classList.toggle("menuShow");
+
+if(menuItemsList.classList.contains("menuShow")){
+hamBtn.innerHTML="Close Menu"; //change button text to chose menu
+}else{ //if menu NOT showing
+hamBtn.innerHTML="Open Menu"; //change button text open menu
+
+}
+
+}
+
+//Display Pizza Info
+function DisplayThisPizzaInfo(id)
+{
+//change pizza Image
+   onePizzaImg.src = "images/Pizzas/Pizza"+id+".png"
+   clickSound.play();
+
+   switch (id)
+   {
+case 1:
+   pizzaNametext.innerHTML = "Neapolitan Pizza (Italy)";
+   crustInfotext.innerHTML = "Thin, soft, slightly charred; puffy edges";
+   toppingInfotext.innerHTML = "Tomatoes, mozzarella di bufala, basil, olive oil";
+break;
+case 2:
+   pizzaNametext.innerHTML = "New York-Style Pizza (USA)";
+   crustInfotext.innerHTML = "Large, thin, and foldable; crisp edges";
+   toppingInfotext.innerHTML = "Tomato sauce, mozzarella, various meats and veggies";
+break;
+case 3:
+   pizzaNametext.innerHTML = "Chicago Deep-Dish Pizza (USA)";
+   crustInfotext.innerHTML = "Deep, buttery, pie-like crust";
+   toppingInfotext.innerHTML = "Cheese on bottom, then meat/veggies, chunky tomato sauce on top";
+break;
+case 4:
+   pizzaNametext.innerHTML = "Sicilian Pizza (Italy / USA)";
+   crustInfotext.innerHTML = "Thick, square, focaccia-style";
+   toppingInfotext.innerHTML = "Tomato sauce, mozzarella, herbs; sometimes anchovies";
+break;
+case 5:
+   pizzaNametext.innerHTML = "Detroit-Style Pizza (USA)";
+   crustInfotext.innerHTML = "Thick, rectangular, crispy cheesy edges";
+   toppingInfotext.innerHTML = "Wisconsin brick cheese, sauce ladled on top";
+break;
+case 6:
+   pizzaNametext.innerHTML = "California-Style Pizza (USA)";
+   crustInfotext.innerHTML = "Thin and crisp";
+   toppingInfotext.innerHTML = "Gourmet and fresh (arugula, goat cheese, smoked salmon, etc.)";
+break;
+case 7:
+   pizzaNametext.innerHTML = "Greek Pizza (USA)";
+   crustInfotext.innerHTML = "Oily, chewy, pan-cooked";
+   toppingInfotext.innerHTML = "Tomato sauce with oregano, feta, olives, red onion";
+break;
+case 8:
+   pizzaNametext.innerHTML = " St. Louis-Style Pizza (USA)";
+   crustInfotext.innerHTML = "Ultra-thin, cracker-like";
+   toppingInfotext.innerHTML = "Provel cheese (cheddar + Swiss + provolone)";
+break;
+   }
+
+   
+}
+
+function NextPizza() 
+{
+  if (pizzaIndex < 8){
+  pizzaIndex = parseInt(pizzaIndex) + 1;
+  DisplayThisPizzaInfo(pizzaIndex);
+  console.log(pizzaIndex);
+  }
+}
+
+function BackPizza() 
+{
+  if (pizzaIndex > 1){
+  pizzaIndex = parseInt(pizzaIndex) - 1;
+  DisplayThisPizzaInfo(pizzaIndex);
+  console.log(pizzaIndex);
+  }
+}
+
+//Display Poster
 function DisplayThisPoster(shownum) {
+
+   posterSound.play();
+
   switch (shownum) {
     case 0:
       showImage.src = "images/reallydoughLandscape.png";
@@ -100,9 +235,7 @@ break;
       break;
   }
 
-  //Poster Landscape Animation
-  PlayImageFade();
-
+PlayImageFade();
 }
 
 function PlayImageFade(){
@@ -114,298 +247,6 @@ function PlayImageFade(){
   }, 500)
 
 }
-
-function hideall(){ //function to hide all pages
-    for (let onepage of allpage){
-      onepage.style.display="none";
-    }
-}
-
-function hideallandShowMinigameButton(){ //function to hide all pages
-    for (let onepage of allpage){
-      onepage.style.display="none";
-    }
-startMinigameButton.style.display = "flex";
-resultscreen.style.display = "none";
-navigation.style.display = "flex";
-
-}
-
-function startMinigame()
-{
-  minigameWindow.style.display = "flex";
-startMinigameButton.style.display = "none";
-navigation.style.display = "none";
-
-   //SetButtons
-   if(buttonIsSet == false){
-    AnswersBtn[0].addEventListener("click", function(){SelectAnswer(0)});
-    AnswersBtn[1].addEventListener("click", function(){SelectAnswer(1)});
-    AnswersBtn[2].addEventListener("click", function(){SelectAnswer(2)});
-    AnswersBtn[3].addEventListener("click", function(){SelectAnswer(3)});
-    buttonIsSet = true;
-   }
-scorevalue = 0;
-score.innerHTML = parseInt(scorevalue) + "/10";
-
-turns = 0;
-randomNum = Math.floor(Math.random() * 8);
-GenerateQuestion(randomNum);
-clearInterval(idleInterval);
-
-characterState(0);
-}
-
-function showPage(pagenum){
-    hideall();
-    let onepage = document.querySelector("#page" + pagenum);
-    onepage.style.display="flex";
-startMinigameButton.style.display = "none";
-}
-
-function GenerateQuestion(rdmQuestion)
-{
-
-  switch (rdmQuestion)
-  {
-    case 0:
-        //SetQuestion
-    correctAnswerId = 1;
-    questions.innerHTML = "Show me a picture of a Hawaiian Pizza"
-    //SetImage
-    imgAnswers[0].src = "images/Pizzas/Pizza1.png";
-    imgAnswers[1].src = "images/Pizzas/Pizza9.png";
-    imgAnswers[2].src = "images/Pizzas/Pizza8.png";
-    imgAnswers[3].src = "images/Pizzas/Pizza3.png";
-
-      break;
-
-    case 1:
-        //SetQuestion
-    correctAnswerId = 3;
-    questions.innerHTML = "Show me a picture of a St. Louis-Style Pizza"
-    //SetImage
-    imgAnswers[0].src = "images/Pizzas/Pizza1.png";
-    imgAnswers[1].src = "images/Pizzas/Pizza3.png";
-    imgAnswers[2].src = "images/Pizzas/Pizza2.png";
-    imgAnswers[3].src = "images/Pizzas/Pizza8.png";
-
-      break;
-
-    case 2:
-        //SetQuestion
-    correctAnswerId = 2;
-    questions.innerHTML = "Show me a picture of a California-Style Pizza"
-    //SetImage
-    imgAnswers[0].src = "images/Pizzas/Pizza2.png";
-    imgAnswers[1].src = "images/Pizzas/Pizza6.png";
-    imgAnswers[2].src = "images/Pizzas/Pizza7.png";
-    imgAnswers[3].src = "images/Pizzas/Pizza4.png";
-
-      break;
-
-    case 3:
-        //SetQuestion
-    correctAnswerId = 0;
-    questions.innerHTML = "Show me a picture of a Neapolitan Pizza"
-    //SetImage
-    imgAnswers[0].src = "images/Pizzas/Pizza1.png";
-    imgAnswers[1].src = "images/Pizzas/Pizza3.png";
-    imgAnswers[2].src = "images/Pizzas/Pizza8.png";
-    imgAnswers[3].src = "images/Pizzas/Pizza2.png";
-
-      break;
-
-    case 4:
-        //SetQuestion
-    correctAnswerId = 2;
-    questions.innerHTML = "Show me a picture of a New York-Style Pizza"
-    //SetImage
-    imgAnswers[0].src = "images/Pizzas/Pizza1.png";
-    imgAnswers[1].src = "images/Pizzas/Pizza3.png";
-    imgAnswers[2].src = "images/Pizzas/Pizza2.png";
-    imgAnswers[3].src = "images/Pizzas/Pizza6.png";
-
-      break;
-
-    case 5:
-        //SetQuestion
-    correctAnswerId = 3;
-    questions.innerHTML = "Show me a picture of a Sicilian Pizza"
-    //SetImage
-    imgAnswers[0].src = "images/Pizzas/Pizza3.png";
-    imgAnswers[1].src = "images/Pizzas/Pizza5.png";
-    imgAnswers[2].src = "images/Pizzas/Pizza7.png";
-    imgAnswers[3].src = "images/Pizzas/Pizza4.png";
-
-      break;
-    case 6:
-       //SetQuestion
-    correctAnswerId = 0;
-    questions.innerHTML = "Show me a picture of a Neapolitan Pizza"
-    //SetImage
-    imgAnswers[0].src = "images/Pizzas/Pizza1.png";
-    imgAnswers[1].src = "images/Pizzas/Pizza3.png";
-    imgAnswers[2].src = "images/Pizzas/Pizza8.png";
-    imgAnswers[3].src = "images/Pizzas/Pizza2.png";
-
-      break;
-    case 7:
-    //SetQuestion
-    correctAnswerId = 3;
-    questions.innerHTML = "Show me a picture of a Detroit Style Pizza"
-    //SetImage
-    imgAnswers[0].src = "images/Pizzas/Pizza4.png";
-    imgAnswers[1].src = "images/Pizzas/Pizza7.png";
-    imgAnswers[2].src = "images/Pizzas/Pizza3.png";
-    imgAnswers[3].src = "images/Pizzas/Pizza5.png";
-
-    break;
-    case 8:
-    //SetQuestion
-    correctAnswerId = 2;
-    questions.innerHTML = "Show me a picture of a Deep Dish Pizza"
-    //SetImage
-    imgAnswers[0].src = "images/Pizzas/Pizza1.png";
-    imgAnswers[1].src = "images/Pizzas/Pizza2.png";
-    imgAnswers[2].src = "images/Pizzas/Pizza3.png";
-    imgAnswers[3].src = "images/Pizzas/Pizza4.png";
-    break;
-  
-  }
-
-
-}
-
-function loopingIdleAnimation() {
-  let index = 1;
-  character.src = "images/Idle" + index + ".png";
-
-  idleInterval = setInterval(() => {
-    index = index >= 3 ? 1 : index + 1;
-    character.src = "images/Idle" + index + ".png";
-  }, 300);
-}
-
-function characterState(state) {
-
-  switch (state) {
-    case 0:
-      loopingIdleAnimation();
-      break;
-
-    case 1:
-      character.src = "images/Correct.png";
-clearInterval(idleInterval);
-      break;
-    case 2:
-      character.src = "images/Wrong.png";
-clearInterval(idleInterval);
-
-      break;
-  }
-}
-
-
-function SelectAnswer(answerId) {
-  if (turns < 10){
-  if (answerId === correctAnswerId)
-  {
-    correctPrompt.style.display = "flex";
-    scorevalue++;
-    score.innerHTML = parseInt(scorevalue) + "/10";
-    characterState(1);
-
-    setTimeout(function () {
-      correctPrompt.style.display = "none";
-   
-    characterState(0);
-      
-
-    }, 500);
-
-  } 
-  else {
-    
-        wrongPrompt.style.display = "flex";
-    characterState(2);
-
-         setTimeout(function () {
-      wrongPrompt.style.display = "none";
- 
-    characterState(0);
-      
-
-    }, 500);
-
-  }
-  turns++;
-
-  randomNum = Math.floor(Math.random() * 8);
-  GenerateQuestion(randomNum);
-}
-else{
-resultscreen.style.display = "flex";
-minigameWindow.style.display = "none";
-clearInterval(idleInterval);
-
-
-    totalscoretext.innerHTML = parseInt(scorevalue) + "/10";
-    if (scorevalue >= 2){
-      ranktext.innerHTML = "D";
-    }
-      if (scorevalue >= 4){
-      ranktext.innerHTML = "C";
-    }
-      if (scorevalue >= 6){
-      ranktext.innerHTML = "B";
-    }
-      if (scorevalue >= 8){
-      ranktext.innerHTML = "A";
-    }
-      if (scorevalue >= 10){
-      ranktext.innerHTML = "S";
-    }
-
-}
-}
-
-
-
-/*Listen for clicks on the buttons, assign anonymous
-eventhandler functions to call show function*/
-page1btn.addEventListener("click", function () {
-showPage(1);
-});
-page2btn.addEventListener("click", function () {
-showPage(2);
-
-});
-page3btn.addEventListener("click", function () {
-showPage(3);
-
-});
-hideall(); //call hideall function to hide all pages
-
-
-
-const menuItemsList=document.querySelector("nav ul");
-hamBtn.addEventListener("click",toggleMenus);
-
-function toggleMenus(){ 
-
-menuItemsList.classList.toggle("menuShow");
-
-if(menuItemsList.classList.contains("menuShow")){
-hamBtn.innerHTML="Close Menu"; //change button text to chose menu
-}else{ //if menu NOT showing
-hamBtn.innerHTML="Open Menu"; //change button text open menu
-
-}
-
-}
-
-
 
 document.addEventListener('keydown', function (kbEvt) {
 //kbEvt: an event object passed to callback function
